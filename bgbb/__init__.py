b7373b7f31cf744a7ad7441963ed36a559e76c7c
wcbeard <wcbeard10@gmail.com>
2019-03-21 13:14:39 -0400

    put version info in _version.py
    python 3.6.5 bump
    
    lower required numba version
    
    version restriction explanations

--- bgbb/__init__.py
+++ bgbb/__init__.py
@@ -1,1 +1,2 @@
 from bgbb.core import BGBB
+from bgbb._version import __version__
--- setup.py
+++ setup.py
@@ -1,12 +1,34 @@
-from setuptools import setup, find_packages
+import re
+from setuptools import find_packages, setup
+
+
+def read_version():
+    "https://stackoverflow.com/a/7071358/386279"
+    VERSIONFILE = "bgbb/_version.py"
+
+    with open(VERSIONFILE, "rt") as fp:
+        verstrline = fp.read()
+    VSRE = r"^__version__ = ['\"]([^'\"]*)['\"]"
+    mo = re.search(VSRE, verstrline, re.M)
+    if mo:
+        verstr = mo.group(1)
+    else:
+        raise RuntimeError(
+            "Unable to find version string in %s." % (VERSIONFILE,)
+        )
+    return verstr
+
+
+verstr = read_version()
 
 setup(
     name="bgbb",
-    version="0.1.4a1",
+    version=verstr,
     packages=find_packages(),
     install_requires=[
         "lifetimes==0.9",
-        "numba==0.42.0",
+        # See Readme for databricks library restrictions
+        "numba>=0.34,<=0.38",
         "scipy",
         "pandas",
         "numpy",
@@ -15,5 +37,5 @@ setup(
     ],
     license=["Apache 2.0", "MIT"],
     extras_require={"test": ["pytest"]},
-    classifiers=["Programming Language :: Python :: 3.5"],
+    classifiers=["Programming Language :: Python :: 3.6"],
 )
