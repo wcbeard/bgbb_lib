8f96b2e5e90fd989a8ebe7d8da6b6b1d57ee6efb
wcbeard <wcbeard10@gmail.com>
2019-04-29 11:34:45 -0400

    udfs and tests for p_x_interval

--- bgbb/sql/bgbb_udfs.py
+++ bgbb/sql/bgbb_udfs.py
@@ -10,11 +10,7 @@ def mk_p_alive_udf(bgbb: BGBB, params=None, alive_n_days_later=0):
     @pandas_udf("double", PandasUDFType.SCALAR)
     def p_alive(frequency, recency, n, params=params):
         arr = bgbb.cond_prob_alive_nb(
-            frequency,
-            recency,
-            n,
-            params=params,
-            n_days_later=alive_n_days_later,
+            frequency, recency, n, params=params, n_days_later=alive_n_days_later
         )
         return Series(arr)
 
@@ -51,9 +47,7 @@ def mk_p_th_exp_udf(bgbb: BGBB, params: Optional[AbgdParams] = None):
     return p_th_est, p_est, th_est
 
 
-def add_p_th(
-    bgbb: BGBB, dfs, params=None, fcol="Frequency", rcol="Recency", ncol="N"
-):
+def add_p_th(bgbb: BGBB, dfs, params=None, fcol="Frequency", rcol="Recency", ncol="N"):
     """
     Use udfs from `mk_p_th_exp_udf` to create columns `p` and `th`
     from intermediate computed column.
@@ -65,4 +59,38 @@ def add_p_th(
         .withColumn("th", th_est(C("p_th")))
         .drop("p_th")
     )
+    return res
+
+
+def mk_prob_never_interval_udf(
+    bgbb: BGBB, params: Optional[AbgdParams] = None, n_days_future=28
+):
+    "UDF for the probability of *ever* returning in next `n_days_future` days"
+    params = params or bgbb.Params
+    print(bgbb)
+
+    @pandas_udf("double", PandasUDFType.SCALAR)
+    def prob_never_interval(f, r, t):
+        p_never = bgbb.p_x_interval(
+            params, f, r, t, n_star=n_days_future, x_star=0, ret_log=False
+        )
+        return Series(1 - p_never)
+
+    return prob_never_interval
+
+
+def add_mau(
+    bgbb: BGBB,
+    dfs,
+    params: Optional[AbgdParams] = None,
+    fcol="Frequency",
+    rcol="Recency",
+    ncol="N",
+    n_days_future=28,
+):
+    prob_never_interval = mk_prob_never_interval_udf(
+        bgbb, params=params, n_days_future=n_days_future
+    )
+
+    res = dfs.withColumn("prob_mau", prob_never_interval(C(fcol), C(rcol), C(ncol)))
     return res
